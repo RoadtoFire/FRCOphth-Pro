220229a207a5e7161c5ef8cba40f6c7a1e572a7c
RoadtoFire <fire.jawwad@gmail.com>
2026-04-18 18:55:56 +0500

    Add CSRF trusted origins

--- MedicalQuiz/settings.py
+++ MedicalQuiz/settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-only')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://frcophth-pro-production.up.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
